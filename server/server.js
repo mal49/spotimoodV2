d9a27aae9d1282561e76218b78fec46c52c20aa6
mal49 <ikhmalhanif60@gmail.com>
2025-07-06 15:08:57 +0800

    initial commit

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors({
     origin: process.env.NODE_ENV === 'production' 
         ? [
-            'https://spotimood-alpha.vercel.app/',  // We'll update this after Vercel deployment
+            'https://spotimood-green.vercel.app/',  // We'll update this after Vercel deployment
             process.env.FRONTEND_URL, // Alternative domain
             /\.vercel\.app$/  // Allow any Vercel subdomain
           ] 
